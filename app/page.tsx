9b5974e5ffae1bfb4ec1a9b721de45ba64dfb21b
Kyn Chong <kyn.chong@gmail.com>
2023-05-25 20:21:22 -0700

    Overhaul

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useInView } from "framer-motion";
-import { MouseEvent, MutableRefObject, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
